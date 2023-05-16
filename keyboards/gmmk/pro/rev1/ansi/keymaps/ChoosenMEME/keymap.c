c28cbcb52d757c516567eaf17f4972ef617969c8
ChoosenMEME <24540327+ChoosenMEME@users.noreply.github.com>
2023-05-16 15:21:00 +0200

    updated keymap top match key legends

--- keyboards/gmmk/pro/rev1/ansi/keymaps/ChoosenMEME/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/ChoosenMEME/keymap.c
@@ -46,25 +46,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_DEF] = LAYOUT(
         KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   KC_DEL,           KC_MUTE,
-        CU_GRV,    KC_1,    KC_2,    CU_3,    KC_4,    KC_5,    CU_6,    CU_7,    CU_8,    CU_9,    CU_0,    KC_SLSH,  CU_EQL,   KC_BSPC,          KC_PGUP,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    DE_Y,    KC_U,    KC_I,    KC_O,    KC_P,    CU_LBRC,  CU_RBRC,  CU_BSLS,          KC_PGDN,
-        OSL(_UML), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    CU_SCLN, CU_QUOT,            KC_ENT,           KC_END,
+        CU_GRV,    KC_1,    KC_2,    CU_3,    KC_4,    KC_5,    CU_6,    CU_7,    CU_8,    CU_9,    CU_0,    KC_SLSH,  CU_EQL,   KC_BSPC,          KC_HOME,
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    DE_Y,    KC_U,    KC_I,    KC_O,    KC_P,    CU_LBRC,  CU_RBRC,  CU_BSLS,          KC_PGUP,
+        OSL(_UML), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    CU_SCLN, CU_QUOT,            KC_ENT,           KC_PGDN,
         CU_LSFT,            DE_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    CU_COMM, CU_DOT,  CU_SLSH,            CU_RSFT, KC_UP,   TG(_NOMOD),
         KC_LCTL,   KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_UML), KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_NOMOD] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_DEL,           KC_MUTE,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_BSPC,          KC_PGUP,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,    KC_BSLS,          KC_PGDN,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,           KC_END,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_BSPC,          KC_HOME,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,    KC_BSLS,          KC_PGUP,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,           KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT, KC_UP,   TG(_NOMOD),
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_UML), KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_UML] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_END ,
         _______, _______, _______, _______, _______, _______, _______, DE_UDIA, _______, DE_ODIA, _______, _______, _______, QK_BOOT,          _______,
         _______, DE_ADIA, DE_SS,   _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, RGB_TOG,
